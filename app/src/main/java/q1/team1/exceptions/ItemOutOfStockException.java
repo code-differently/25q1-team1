cf1545f93f002bca73a74354f656dfe4e04fd425
jxwatson251 <jxwatson251@gmail.com>
2025-04-18 12:14:28 -0400

    implement exceptions along with Item class with ItemTest (#7)

---
+++ app/src/main/java/q1/team1/exceptions/ItemOutOfStockException.java
@@ -0,0 +1,11 @@
+package q1.team1.exceptions;
+
+public class ItemOutOfStockException extends Exception{
+
+    public ItemOutOfStockException() {}
+    
+    public ItemOutOfStockException(String message) {
+        super(message);
+  }
+
+}
