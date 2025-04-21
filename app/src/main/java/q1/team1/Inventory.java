5c4938b0d4395483e7c02c2663d38fe6bb5d70a3
mercedes-mathews <89824585+mercedes-mathews@users.noreply.github.com>
2025-04-21 11:48:57 -0400

    Feat: inventory interface (#23)
    * feat: adds an Inventory interface
    
    * fix: adjusts Inventory, Employee, and InventoryTest to use Inventory interface
    
    * fix: adjusts test to use inventory interface

--- app/src/main/java/q1/team1/Inventory.java
+++ app/src/main/java/q1/team1/Inventory.java
@@ -5,10 +5,11 @@ import java.util.Map;
 import q1.team1.exceptions.ItemNotFoundException;
 import q1.team1.exceptions.ItemOutOfStockException;
 
-public class Inventory {
+public class Inventory implements IInventory {
   private final Map<String, Item> itemByItemID = new HashMap<>();
   private final Map<String, Integer> stockByItemId = new HashMap<>();
 
+  @Override
   public Item getItem(String itemId) {
     if (!stockByItemId.containsKey(itemId)) {
       throw new ItemNotFoundException("Item does not exist");
@@ -16,6 +17,7 @@ public class Inventory {
     return itemByItemID.get(itemId);
   }
 
+  @Override
   public int getStockOfItem(String itemId) {
     if (!stockByItemId.containsKey(itemId)) {
       throw new ItemNotFoundException("Cannot get stock. Item does not exist");
@@ -23,6 +25,7 @@ public class Inventory {
     return stockByItemId.get(itemId);
   }
 
+  @Override
   public void addStock(String itemId, Integer quantity) {
     if (!stockByItemId.containsKey(itemId)) {
       throw new ItemNotFoundException("Cannot add stock. Item does not exist");
@@ -30,11 +33,13 @@ public class Inventory {
     stockByItemId.put(itemId, stockByItemId.get(itemId) + quantity);
   }
 
+  @Override
   public void addItem(Item item, Integer quantity) {
     stockByItemId.put(item.getID(), quantity);
     itemByItemID.put(item.getID(), item);
   }
 
+  @Override
   public boolean reserveItem(String itemId, int quantity) {
     if (!stockByItemId.containsKey(itemId)) {
       throw new ItemNotFoundException("Item does not exist");
