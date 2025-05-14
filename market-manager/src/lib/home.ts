232df2fc20bb075f343a9603e19acffc7b830d28
jxwatson251 <jxwatson251@gmail.com>
2025-05-14 18:24:21 +0000

    invetory

--- market-manager/src/lib/home.ts
+++ market-manager/src/lib/home.ts
@@ -1,1 +1,40 @@
 // Functions to interact with the 'items' collection (getAvailableItems, reserveItem)
+import { Item } from './Item';
+
+export class Inventory {
+    items: Item[] = [];
+
+    addItem(item: Item) {
+        this.items.push(item);
+    }
+
+    removeItem(id: string) {
+        this.items = this.items.filter(item => item.id !== id);
+    }
+
+    getItem(id: string): Item | undefined{
+        return this.items.find(item => item.id === id);
+    }
+
+    updateItem(updatedItem: Item): void {
+        this.items = this.items.map(item => item.id === updatedItem.id ? updatedItem : item);
+    }
+
+    listItems(): Item[] {
+        return this.items;
+    }
+
+    getAvailableItems(): Item[] {
+        return this.items.filter(item => item.available);
+    }
+
+    reserveItem(id: string): boolean {
+        const item = this.getItem(id);
+        if (item && item.available) {
+            item.available = false;
+            this.updateItem(item);
+            return true;
+        }
+        return false;
+    }
+}
