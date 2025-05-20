fec40766f3c69d0f21dbe0119fb123e7af1bda85
jxwatson251 <jxwatson251@gmail.com>
2025-05-19 21:35:14 -0400

    feat: implement cart function  (#52)
    * feat: add cart.ts
    
    * cart ts
    
    * cart
    
    * cart
    
    * corrections
    
    * implement cart
    
    * implement changes
    
    * make changes to cart
    
    * implement changes
    
    * chore: clean code
    
    * feat: adjusted test cases
    
    ---------
    
    Co-authored-by: mercedes-mathews <memathews251@gmail.com>

--- market-manager/src/types/cart.ts
+++ market-manager/src/types/cart.ts
@@ -1,1 +1,11 @@
 // Interface for Cart (customer ID, list of reserved items, timestamps, etc.)
+
+import { Timestamp } from "firebase/firestore";
+
+export interface Cart {
+ customerId: string;
+ product: string[];
+ createdAt: Timestamp;
+ updatedAt: Timestamp;
+ totalPrice?: number; // optional, if you want to cache it
+}
