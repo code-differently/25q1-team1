79ef4c55b855cadbc41e72a26f9604c49c030fbb
mercedes-mathews <89824585+mercedes-mathews@users.noreply.github.com>
2025-05-20 09:50:40 -0400

    feat: adding extra jest dependacies to track 80% code coverage (#58)
    * feat: cart is connected to the current customer and firebase
    
    * feat: adding extra jest dependacies to track 80% code coverage

--- market-manager/jest.config.js
+++ market-manager/jest.config.js
@@ -5,4 +5,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: defaults.transform, // <-- This is how to access ts-jest transform config
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/lib/**/*.{ts,tsx,js}',
+    '!**/*.test.{ts,tsx,js}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
